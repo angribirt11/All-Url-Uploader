f84a21ecc6377d0d4357ccd759316678bc311237
angribirt11 <thienpro400@gmail.com>
2023-06-27 21:34:59 +0700

    Update config.py

--- Uploader/config.py
+++ Uploader/config.py
@@ -33,12 +33,12 @@ logging.basicConfig(
 
 
 class Config(object):
-    WEBHOOK = os.environ.get("6266997142:AAH5HyeUQNgJB65H9CYWe1btQ9PuEMHD7Y4", False)
+    WEBHOOK = os.environ.get("BOT_TOKEN", False)
     # Get a token from @BotFather
-    BOT_TOKEN = os.environ.get("6266997142:AAH5HyeUQNgJB65H9CYWe1btQ9PuEMHD7Y4")
+    BOT_TOKEN = os.environ.get("BOT_TOKEN", "")
     # The Telegram API things
-    API_ID = int(os.environ.get("23687011")
-    API_HASH = os.environ.get("ecb7e7889511fbb8e754d829a4f8fcfe")
+    API_ID = int(os.environ.get("API_ID", 12345))
+    API_HASH = os.environ.get("API_HASH")
     # Get these values from my.telegram.org
     # Array to store users who are authorized to use the bot
 
@@ -63,10 +63,11 @@ class Config(object):
     # Set timeout for subprcess
     PROCESS_MAX_TIMEOUT = 3700
 
-    LOG_CHANNEL = int(os.environ.get("-956026831")
-    OWNER_ID = int(os.environ.get("911285605")
-    BOT_USERNAME = os.environ.get("google_thien_bot")
+    LOG_CHANNEL = int(os.environ.get("LOG_CHANNEL", -100))
+    OWNER_ID = int(os.environ.get("OWNER_ID", "12356"))
+    BOT_USERNAME = os.environ.get("BOT_USERNAME", "")
     ADL_BOT_RQ = {}
     AUTH_USERS = list({int(x)
-                      for x in os.environ.get("911285605", "0").split()})
+                      for x in os.environ.get("AUTH_USERS", "0").split()})
     AUTH_USERS.append(OWNER_ID)
+1
